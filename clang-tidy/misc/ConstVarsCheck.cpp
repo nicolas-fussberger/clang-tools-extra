b8d1b952318031e2aaa2d03ce693befadf728d4f
Nicolas Fußberger <nicolas.fussberger@web.de>
2019-02-17 19:30:27 +0100

    find every ref of non-const var

--- clang-tidy/misc/ConstVarsCheck.cpp
+++ clang-tidy/misc/ConstVarsCheck.cpp
@@ -9,6 +9,7 @@
 #include "ConstVarsCheck.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "../utils/DeclRefExprUtils.h"
 
 using namespace clang::ast_matchers;
 
@@ -18,24 +19,39 @@ namespace misc {
 
 void ConstVarsCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(
-      varDecl(hasType(qualType(unless(isConstQualified())))).bind("x"), this);
+      functionDecl(
+          forEachDescendant(
+              varDecl(hasType(qualType(unless(isConstQualified())))).bind("x")))
+          .bind("func"),
+      this);
 }
 
 void ConstVarsCheck::check(const MatchFinder::MatchResult &Result) {
   // FIXME: Add callback implementation.
-  const auto *MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("x");
+  const auto *Var = Result.Nodes.getNodeAs<VarDecl>("x");
+  const auto *FuncDecl = Result.Nodes.getNodeAs<FunctionDecl>("func");
 
-  // if variable is already const -> skipped
-//  if(MatchedDecl->getType().isConstQualified()) return;
+
+  auto AllDeclRefExprs = utils::decl_ref_expr::allDeclRefExprs(
+        *Var, *FuncDecl, *Result.Context);
+  auto it = AllDeclRefExprs.begin();
+  while (it != AllDeclRefExprs.end()) {
+    const auto *declPtr = *it;
+    diag(declPtr->getBeginLoc(), "var ref at %0")
+        << declPtr->getDeclName();
+    ++it;
+  }
+//
 
   // Check if variable is reassigned within its scope
 
   // Check if variable is used as a non-const parameter in a function call
 
-
-  diag(MatchedDecl->getLocation(), "var %0 is insufficiently awesome")
-      << MatchedDecl
-      << FixItHint::CreateInsertion(MatchedDecl->getLocation(), "awesome_");
+  diag(FuncDecl->getLocation(), "func %0 contains non-const vars")
+      << FuncDecl;
+  diag(Var->getLocation(), "var %0 is insufficiently awesome")
+      << Var
+      << FixItHint::CreateInsertion(Var->getLocation(), "awesome_");
 }
 
 } // namespace misc
